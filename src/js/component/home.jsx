8bf2b432f0c678d0d4916684551e16d9992df687
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-03-01 20:10:31 +0000

    Proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from './navbar';
+import Jumbotron from './jumbotron';
+import Card from './card';
+import Footer from './footer';
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div>
+		<Navbar />
+		<div className="container mt-4">
+		<Jumbotron />
+		<div className="container mt-4">
+		  <div className="row">
+			<div className="col-md-3 mb-4">
+			  <Card image="https://picsum.photos/500/325"/>
+			</div>
+			<div className="col-md-3 mb-4">
+			  <Card image="https://picsum.photos/500/325"/>
+			</div>
+			<div className="col-md-3 mb-4">
+			  <Card image="https://picsum.photos/500/325"/>
+			</div>
+			<div className="col-md-3 mb-4">
+			  <Card image="https://picsum.photos/500/325"/>
+			</div>
+		  </div>
 		</div>
+		</div>
+		<Footer />
+	  </div>
 	);
-};
-
-export default Home;
+  };
+  
+  export default Home;
